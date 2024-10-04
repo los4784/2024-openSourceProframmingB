bba9e4e144ed9852a0a0e4f6a2600ab70c8c1124
los4784 <los4784@naver.com>
2024-10-04 17:58:23 +0900

    v0.5

--- week06/main.go
+++ week06/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-   "fmt"
+	"fmt"
+	"reflect"
 )
-Func main() {
+
+func main() {
 	i := 13
-	f := float64 = 12.9 // f:= 12.9
-	
+	var f float64 = 12.9 // f:= 12.9
+	c1 := '2'
+	c2 := '김'
+
 	fmt.Printf("value i : %d, value f : %f\n", i, f)
 	//fmt.Printf("%d * %f = %f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
-	fmt.Printf("%d * %f = %f\n", i, f, i*int*f) // conversion
-	fmt.Printf(reflect.TypeOf(f),reflect.TypeOf(i))
+	fmt.Printf("%d * %f = %f\n", i, f, i*int(f)) // conversion
+	fmt.Println(c1, c2)
+	fmt.Printf("%x\n", c2)
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
 }
