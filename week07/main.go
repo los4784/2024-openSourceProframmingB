69e5ab231869841c1f6393cf4b6d32ff3dc17d9f
los4784 <los4784@naver.com>
2024-10-11 17:23:55 +0900

    v1.0 if

--- week07/main.go
+++ week07/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"time"
+	"log"
+	"os"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	fmt.Printf("오늘은 %d년 %d월 %d일 입니다\n",now.Year(),int(now.Month()),now.Day())
-	fmt.Printf("지금 시각은 %d시 %d분 %d초 입니다\n",now.Hour(),int(now.Minute()),now.Second())
+	fmt.Print("이름 입력 : ")
+	r := bufio.NewReader(os.Stdin)
+	name, err := r.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
 
 }
