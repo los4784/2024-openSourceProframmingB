f80bd0438d93013b577680e811280753bb2269a9
los4784 <los4784@naver.com>
2024-10-11 17:04:10 +0900

    v0.8

--- week06/main.go
+++ week06/main.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	i := 13
-	var f float64 = 12.9 // f:= 12.9
-	c1 := '2'
-	c2 := '김'
+	var f float64
+	var i int
+	var b bool
+	var s string
 
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	//fmt.Printf("%d * %f = %f\n", i, f, i*f) // invalid operation: i * f (mismatched types int and float64)
-	fmt.Printf("%d * %f = %f\n", i, f, i*int(f)) // conversion
-	fmt.Println(c1, c2)
-	fmt.Printf("%x\n", c2)
-	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i)
+
+	i = 3
+	f = 2.7
+	fmt.Print("\n\n", f <= float64(i), "\n")
+
 }
