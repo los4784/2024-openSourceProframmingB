0d41ca802a834d1c0c041bf07ab7b417704892a7
los4784 <los4784@naver.com>
2024-10-25 16:51:40 +0900

    v1.6

--- week09/main.go
+++ week09/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -10,4 +15,26 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	answer := rand.Intn(6) + 1 // dice 1 ~ 6
 	fmt.Println(answer)
+
+	fmt.print("숫자 입력: ")
+	in := bufio.NewReader(os.Stdin)
+	input, err := in.ReadString("/n")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input) // 줄바꿈, 띄엇쓰기 탭 등 제거 python strip과 유사
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(guess)
+
+	if answer == guess {
+		fmt.Println("정답입니다")
+	} else if answer > guess {
+		fmt.Println("입력하신 값은 정답보다 작은 수 입니다. LOW")
+	} else { // answer < guess
+		fmt.Println("입력하신 값은 정답보다 큰 수 입니다.")
+	}
 }
