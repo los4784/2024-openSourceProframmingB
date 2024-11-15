4f92144bfa4b17b43e6821348d9b6a01678b1857
los4784 <los4784@naver.com>
2024-11-15 17:12:17 +0900

    v1.5 Attay

--- week12/main.go
+++ week12/main.go
@@ -17,8 +17,18 @@ func main() {
 		time.Unix(1, 0),
 		time.Unix(1708012345, 0),
 	}
-	fmt.Println(dates[0], dates[1], dates[2])
-	fmt.Println(dates)          // 배열 찍음
-	fmt.Println("%#v\n", dates) // 배열 리터널 literall
+	//fmt.Println(dates[0], dates[1], dates[2])
+	//fmt.Println(dates)          // 배열 찍음
+	//fmt.Println("%#v\n", dates) // 배열 리터널 literall
+
+	//for i := 0; i <= 2; i++  아래보다 안좋은 코드
+	//for i := 0; i < len(dates); i++ {
+	//	fmt.Println(i, dates[i])
+	for i, date := range dates { // like python for in 안전하게 사용 가능
+		fmt.Println(i, date)
+	}
+	for _, date := range dates { // 인덱스 놉
+		fmt.Println(date)
+	}
 
 }
