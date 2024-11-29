d672c0b4069e68664e2151419694e43121904048
los4784 <los4784@naver.com>
2024-11-29 17:48:03 +0900

    V2.1 Struct

--- week14/main.go
+++ week14/main.go
@@ -3,24 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	ages := make(map[string]int)
-
-	var name string
-	var age int
-
-	for {
-		fmt.Print("Name : ")
-		fmt.Scanln(&name)
-		if name == "q" {
-			break
-		}
-		fmt.Print("Age : ")
-		fmt.Scanln(&age)
-
-		ages[name] = age
-
+	var student struct {
+		id   int
+		name string
+		gpa  float32
 	}
-	for k, v := range ages {
-		fmt.Printf("%s is %d years old\n", k, v)
+
+	student.id = 20241234
+	student.name = "Son Heungmin"
+	student.gpa = 4.5
+	fmt.Println(student.gpa)
+	var student2 struct {
+		id   int
+		name string
+		gpa  float32
 	}
+	student.id = 20244321
+	student.name = "Seo Jihye"
+	student.gpa = 4.43
+	fmt.Println(student2.id)
 }
